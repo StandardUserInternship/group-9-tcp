deb3cc335ba2e0693e77041181133dedb6f1f6ce
Jon <j.rosett@standarduserllc.com>
2022-06-22 12:38:02 -0500

    demo stashing

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import socket
 import threading
+import requests
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
