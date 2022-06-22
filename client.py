6346f5c36c0c84d1c245f1f67d6ebbc758e22a36
Narvik86 <hxk151830@utdallas.edu>
2022-06-22 12:14:58 -0500

    temp

---
+++ client.py
@@ -0,0 +1,45 @@
+# client.py
+
+import socket
+import thread
+import argparse
+import sys
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+def receive(s):
+    while True:
+        data = s.recv(1024).decode('ascii')
+        print(data)
+
+def main(argv):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        userName = argv
+        s.connect((HOST, PORT))
+        s.sendall(f"{userName} entered to the chat room".encode('ascii'))
+        
+        thread.start_new_thread(receive,(s))
+
+        while True:
+            userInput = str(input())
+
+            if (userInput.startswith('!LIST' or '!list')):
+                s.send('list').encode('ascii')
+
+            elif (userInput.startswith('!QUIT'or '!quit')):
+                s.send(f"{userName} disconnected")
+            
+            elif (userInput.startswith('!HELP'or '!help')):
+                print("Useable commands:")
+                print("<message>: Sends the message to all connected users")
+                print("!LIST: display list of all users")
+                print("!QUIT: quit chatting room")
+            else :
+                s.send(userInput)
+            
+
+            s.sendall(userInput)
+        
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1]))
---
+++ server.py
@@ -0,0 +1,51 @@
+# server.py
+
+import socket
+import thread
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+userList = []
+
+def on_new_client(conn,addr):
+
+    print(f"Connected by {addr}")
+
+    data = conn.recv(1024).decode('ascii')
+    data.split(" ")
+    userName = data[0]
+    global userList
+    userList.append(userName) 
+ 
+    while True:
+        data = conn.recv(1024).decode('ascii')
+        if data == "!QUIT":
+            break
+
+        elif data == "!LIST":
+            conn.send(userList)
+
+        else:
+            conn.sendall(data.encode('ascii'))
+
+    conn.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, PORT))
+s.listen(5)
+
+print ('Server started!')
+print ('Waiting for clients...')
+
+s.bind((HOST, PORT))        # Bind to the port
+s.listen(5)   
+
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   thread.start_new_thread(on_new_client,(c,addr))
+  
+   
+
+
