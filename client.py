05d129dbfb8c33053b99e5e02bdbcf36b3ef8b0c
Narvik86 <hxk151830@utdallas.edu>
2022-06-26 01:35:56 -0500

    recent

--- client.py
+++ client.py
@@ -9,37 +9,42 @@ HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 def receive(s):
-    while True:
-        data = s.recv(1024).decode('ascii')
+    print("here is receive")
+    while True:        
+        data = s.recv(1024).decode()
         print(data)
 
 def main(argv):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         userName = argv
         s.connect((HOST, PORT))
-        s.sendall(f"{userName} entered to the chat room".encode('ascii'))
+        s.sendall(f"{userName} entered to the chat room".encode())
         
-        threading.start_new_thread(receive,(s))
+        x = threading.Thread(target=receive, args=(s,))
+        x.start()
 
         while True:
-            userInput = str(input())
-
-            if (userInput.startswith('!LIST' or '!list')):
-                s.send('list').encode('ascii')
-
-            elif (userInput.startswith('!QUIT'or '!quit')):
-                s.send(f"{userName} disconnected")
+            userInput = str(input("enter a message: "))
             
-            elif (userInput.startswith('!HELP'or '!help')):
+
+            if (userInput.startswith("!LIST") or userInput.startswith("!list")):
+                s.send('!LIST'.encode())
+
+            elif (userInput.startswith("!QUIT") or userInput.startswith("!quit")):
+                s.send(f"{userName} disconnected".encode())
+                print("Connection closed")
+                s.close()
+                sys.exit()
+            
+            elif (userInput.startswith("!HELP") or userInput.startswith("!help")):
                 print("Useable commands:")
                 print("<message>: Sends the message to all connected users")
                 print("!LIST: display list of all users")
                 print("!QUIT: quit chatting room")
             else :
-                s.send(userInput)
+                s.send(userInput.encode())
             
 
-            s.sendall(userInput)
         
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1]))
--- server.py
+++ server.py
@@ -13,23 +13,33 @@ def on_new_client(conn,addr):
     print(f"Connected by {addr}")
 
     data = conn.recv(1024).decode()
-    data.split(" ")
+    print(data)
+    data = data.split()
     userName = data[0]
     global connDict
     connDict[conn] = userName
+    
  
     while True:
-        data = conn.recv(1024).decode('ascii')
+        data = conn.recv(1024).decode()
+        if (data):
+            print(data)
+
         if data == "!QUIT":
             break
 
         elif data == "!LIST":
-            conn.send(connDict.values()).encode()
+            list = ""
+            for key, value in connDict.items():
+                list = list + str(value) + ", "
+            list = list[:len(list)-2]
+            conn.send(list.encode())
 
         else:
-            conn.sendall(data.encode('ascii'))
+            conn.send(data.encode())
 
     conn.close()
+    del connDict[conn]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -41,7 +51,8 @@ print ('Waiting for clients...')
 
 while True:
    c, addr = s.accept()     # Establish connection with client.
-   threading.start_new_thread(on_new_client,(c,addr))
+   x = threading.Thread(target=on_new_client, args=(c,addr))
+   x.start()
   
    
 
